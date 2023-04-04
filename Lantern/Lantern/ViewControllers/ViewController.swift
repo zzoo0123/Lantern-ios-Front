392fbf5e64fc64f1da511df988798fa9d733b456
zzoo0123 <0123suh@gmail.com>
2023-04-04 15:55:59 +0900

    feat: 가입 시작 페이지 추가

--- Lantern/Lantern/ViewControllers/ViewController.swift
+++ Lantern/Lantern/ViewControllers/ViewController.swift
@@ -6,8 +6,10 @@
 //
 
 import UIKit
+import KakaoSDKCommon
 import KakaoSDKAuth
 import KakaoSDKUser
+import AuthenticationServices
 
 class ViewController: UIViewController {
     
@@ -55,5 +57,75 @@ class ViewController: UIViewController {
         self.present(nextVC, animated: true)
     }
     
+    
+//    func setupProviderLoginView() {
+//        let authorizationButton = ASAuthorizationAppleIDButton()
+//            authorizationButton.addTarget(self, action: #selector(clickAppleLoginBtn), for: .touchUpInside)
+//            self.loginProviderStackView.addArrangedSubview(authorizationButton)
+//          }
+//    }
+//
+//
+////    @objc
+//    func clickAppleLoginBtn() {
+//        let appleIDProvider = ASAuthorizationAppleIDProvider()
+//        let request = appleIDProvider.createRequest()
+//        request.requestedScopes = [.fullName, .email]
+//
+//        let controller = ASAuthorizationController(authorizationRequests: [request])
+//        controller.delegate = self
+//        controller.presentationContextProvider = self as? ASAuthorizationControllerPresentationContextProviding
+//        controller.performRequests()
+//    }
+
+//
+//    // kakao 로그인 토큰 발행
+//    func kakaoLogin() {
+//        if (UserApi.isKakaoTalkLoginAvailable()) {
+//            // 카카오톡이 설치되어있는지 확인
+//            UserApi.shared.loginWithKakaoTalk{(oauthToken, error) in
+//                if let error = error {
+//                    print(error)
+//                } else {
+//                    print("login with KakaoTalk success")
+//                    // do something
+//                    _ = oauthToken
+//                }
+//            }
+//
+//            UserApi.shared.loginWithKakaoTalk {(oauthToken, error) in
+//                if let error = error {
+//                    print(error)
+//                } else {
+//                    print("loginWithKakaoTalk() success.")
+//                    //do something
+//                    _ = oauthToken
+//                }
+//            }
+//        }
 }
 
+//extension ViewController: ASAuthorizationControllerDelegate {
+//    // 성공 후 동작
+//    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+//            if let credential = authorization.credential as? ASAuthorizationAppleIDCredential {
+//
+//                let idToken = credential.identityToken!
+//                let tokeStr = String(data: idToken, encoding: .utf8)
+//                print(tokeStr)
+//
+//                guard let code = credential.authorizationCode else { return }
+//                let codeStr = String(data: code, encoding: .utf8)
+//                print(codeStr)
+//
+//                let user = credential.user
+//                print(user)
+//
+//            }
+//        }
+//
+//    // 실패 후 동작
+//    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+//            print("error")
+//    }
+//}
